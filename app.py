013f5a26de0096d5247872e4a27ef3c84683159d
cdang <briannd81@gmail.com>
2018-09-23 01:33:55 -0400

    add

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ import os
 import signal
 from flask import Flask
 from buzz import generator
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
 
 app = Flask(__name__)
 
@@ -13,6 +19,15 @@ def generate_buzz():
     page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
+
+@app.route("/unitconversion"):
+def unitconversion():
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('submit.html', form=form)
+    
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
